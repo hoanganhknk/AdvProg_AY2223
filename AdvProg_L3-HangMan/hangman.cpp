b658a844d883cac73595355f031d78c97a22c6ed
hoanganhknk <hoanganhnguyen.knk@gmail.com>
2023-02-22 11:07:16 +0700

    updatehangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -69,8 +69,10 @@ bool isCharInWord(const char ch, const string& word)
 string chooseWordFromList(const vector<string>& wordList, int index) 
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
-    string answer;
-    answer = wordList[index];
+    string answer = "";
+    for (int i = 0; i < wordList[index]; i++) {
+        answer = answer + tolower(wordList[index][i]);
+    }
     return answer;
 }
 
