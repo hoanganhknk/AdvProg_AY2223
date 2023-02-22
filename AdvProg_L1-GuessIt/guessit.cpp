d871af608504bfbb76fda4cea2fe67d9210f2856
hoanganhknk <hoanganhnguyen.knk@gmail.com>
2023-02-22 10:26:58 +0700

    updateguessit

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -85,7 +85,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued = 'y' || isContinued = 'Y') {
+    if (isContinued == 'y' || isContinued == 'Y') {
         result = true;
     }
     else {
